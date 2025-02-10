6233cd23dce10b1ca6152e14f0d9bbca3b9c1dcb
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2025-02-11 03:25:53 +0800

    update the links

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
       
       "http://127.0.0.1:5500",
       "http://localhost:5173",
-      "https://riko-ai-delta.vercel.app/"
+      "https://riko-ai-delta.vercel.app"
       
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
